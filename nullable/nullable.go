75f5018d1da01d897528a623589bcda10fe0c0f4
Luca Osti <plutonio@gmail.com>
2017-07-26 12:48:17 +0200

    Missing helper methods for nullable float and int types

--- nullable/nullable.go
+++ nullable/nullable.go
@@ -21,3 +21,43 @@ func StringFromPtr(s *string) String {
 func NullString() String {
 	return StringFromPtr(nil)
 }
+
+// Int is a simple alias to null.Int pointer
+type Int *null.Int
+
+// IntFrom is a simple wrapper to get a pointer from guregu/null.IntForm
+func IntFrom(s int64) Int {
+	nullInt := null.IntFrom(s)
+	return &nullInt
+}
+
+// IntFromPtr is a simple wrapper to get a pointer from guregu/null.IntFromPtr
+func IntFromPtr(s *int64) Int {
+	nullInt := null.IntFromPtr(s)
+	return &nullInt
+}
+
+// NullInt returns an invalid null.Int, which will Marshal to a null JSON field
+func NullInt() Int {
+	return IntFromPtr(nil)
+}
+
+// Float is a simple alias to null.Float pointer
+type Float *null.Float
+
+// FloatFrom is a simple wrapper to get a pointer from guregu/null.FloatForm
+func FloatFrom(s float64) Float {
+	nullFloat := null.FloatFrom(s)
+	return &nullFloat
+}
+
+// FloatFromPtr is a simple wrapper to get a pointer from guregu/null.FloatFromPtr
+func FloatFromPtr(s *float64) Float {
+	nullFloat := null.FloatFromPtr(s)
+	return &nullFloat
+}
+
+// NullFloat returns an invalid null.Float, which will Marshal to a null JSON field
+func NullFloat() Float {
+	return FloatFromPtr(nil)
+}
