2df985595347db6a8ed37ec1342240c64561a1f3
Luca Osti <plutonio@gmail.com>
2017-08-01 11:17:36 +0200

    Actual test for Events.List

--- client/events_test.go
+++ client/events_test.go
@@ -17,7 +17,7 @@ func TestEventList(t *testing.T) {
 	setup()
 	defer teardown()
 
-	response := `{"page":{"size":2,"totalElements":10,"totalUnfilteredElements":0,"totalPages":5,"number":0},"elements":[{"id":"my-event-id1","customerId":"my-customer-id","bringBackProperties":null,"type":"viewedProduct","context":"ECOMMERCE","date":"2017-07-13T16:14:46.019+0000","properties":{"id":"999","sku":"test-something","name":"kit bundle (Test)","price":0,"imageUrl":"https://something.jpg","linkUrl":"https://store.example","shortDescription":"Test bundle","category":[]},"contextInfo":{"client":{"ip":"111.111.111.111","userAgent":"Somebrowser/1.1"}},"registeredAt":"2017-07-13T08:19:11.019+0000","updatedAt":"2017-07-13T08:19:11.019+0000","tracking":null},{"id":"my-event-id2","customerId":"my-customer-id","bringBackProperties":null,"type":"reviewedProduct","context":"ECOMMERCE","date":"2017-07-13T16:14:48.019+0000","properties":{"id":"10000","sku":"test-something2","name":"kit bundle2 (Test)","price":0,"imageUrl":"https://something2.jpg","linkUrl":"https://store.example","shortDescription":"Test bundle 2","category":[]},"contextInfo":{"client":{"ip":"111.111.111.111","userAgent":"Somebrowser/1.1"}},"registeredAt":"2017-07-13T08:19:11.01+0000","updatedAt":"2017-07-13T08:19:11.019+0000","tracking":null}]}`
+	response := `{"page":{"size":2,"totalElements":10,"totalUnfilteredElements":0,"totalPages":5,"number":0},"elements":[{"id":"my-event-id1","customerId":"my-customer-id","bringBackProperties":null,"type":"viewedProduct","context":"ECOMMERCE","date":"2017-08-01T20:23:09.215+0000","properties":{"id":"999","sku":"test-something","name":"kit bundle (Test)","price":0,"imageUrl":"https://something.jpg","linkUrl":"https://store.example","shortDescription":"Test bundle","category":[]},"contextInfo":{"client":{"ip":"111.111.111.111","userAgent":"Somebrowser/1.1"}},"registeredAt":"2017-07-13T08:19:11.019+0000","updatedAt":"2017-07-13T08:19:11.019+0000","tracking":null},{"id":"my-event-id2","customerId":"my-customer-id","bringBackProperties":null,"type":"reviewedProduct","context":"ECOMMERCE","date":"2017-08-01T20:23:09.215+0000","properties":{"id":"10000","sku":"test-something2","name":"kit bundle2 (Test)","price":0,"imageUrl":"https://something2.jpg","linkUrl":"https://store.example","shortDescription":"Test bundle 2","category":[]},"contextInfo":{"client":{"ip":"111.111.111.111","userAgent":"Somebrowser/1.1"}},"registeredAt":"2017-07-13T08:19:11.01+0000","updatedAt":"2017-07-13T08:19:11.019+0000","tracking":null}]}`
 	mux.HandleFunc("/events", func(w http.ResponseWriter, r *http.Request) {
 		testMethod(t, r, http.MethodGet)
 		testQueryStringValue(t, r, "customerId", "my-customer-id")
@@ -46,8 +46,67 @@ func TestEventList(t *testing.T) {
 	if diff := pretty.Compare(pageInfo, expectedPageInfo); diff != "" {
 		t.Errorf("Events.List: invalid value for struct: (-got +expected)\n%s", diff)
 	}
+	date1, _ := time.Parse("2006-01-02T15:04:05.999-0700", "2017-08-01T20:23:09.215+0000")
+	date2, _ := time.Parse("2006-01-02T15:04:05.999-0700", "2017-08-01T20:23:09.215+0000")
+	expectedList := []EventResponse{
+		EventResponse{
+			ID: "my-event-id1",
+			Event: &Event{
+				CustomerID: "my-customer-id",
+				Type:       enums.ViewedProduct,
+				Context:    enums.Ecommerce,
+				Properties: map[string]interface{}{
+					"id":               "999",
+					"sku":              "test-something",
+					"name":             "kit bundle (Test)",
+					"price":            0,
+					"imageUrl":         "https://something.jpg",
+					"linkUrl":          "https://store.example",
+					"shortDescription": "Test bundle",
+					"category":         []string{},
+				},
+				ContextInfo: &map[string]interface{}{
+					"client": map[string]interface{}{
+						"ip":        "111.111.111.111",
+						"userAgent": "Somebrowser/1.1",
+					},
+				},
+				Date: &CustomDate{date1},
+			},
+		},
+		EventResponse{
+			ID: "my-event-id2",
+			Event: &Event{
+				CustomerID: "my-customer-id",
+				Type:       enums.ReviewedProduct,
+				Context:    enums.Ecommerce,
+				Properties: map[string]interface{}{
+					"id":               "10000",
+					"sku":              "test-something2",
+					"name":             "kit bundle2 (Test)",
+					"price":            0,
+					"imageUrl":         "https://something2.jpg",
+					"linkUrl":          "https://store.example",
+					"shortDescription": "Test bundle 2",
+					"category":         []string{},
+				},
+				ContextInfo: &map[string]interface{}{
+					"client": map[string]interface{}{
+						"ip":        "111.111.111.111",
+						"userAgent": "Somebrowser/1.1",
+					},
+				},
+				Date: &CustomDate{date2},
+			},
+		},
+	}
+
+	if diff := pretty.Compare(events, expectedList); diff != "" {
+		t.Errorf("Events.List: invalid value for struct: (-got +expected)\n%s", diff)
+	}
 }
 
+// This method actually returns an empty body on the actual ContactHub API, but the documentation states otherwise.
 func TestEventCreate(t *testing.T) {
 	setup()
 	defer teardown()
